5865ad5034727fffdbda61ba31f9d041b3d2157c
Зульфикар Турганов <vonagrut@MacBook-Pro-Zulfikar.local>
2020-10-27 00:11:25 +0600

    final_changes_of_project

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    len_of_original_word = len(user_text.split())
+    return render(request, 'reverse.html', {'user_text': user_text,
+                                            'reversed_text': reversed_text,
+                                            'count_of_word': len_of_original_word})
